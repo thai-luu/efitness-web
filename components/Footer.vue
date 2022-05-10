d2d57b38eeea17a012af13200ee3bb8ee81330ce
thai-luu <thailuu0928@gmail.com>
2022-05-10 23:08:47 +0700

    up footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-        nuxt nhu loz
+    <div class="flex flex-col footer bg-slate-50 pt-12">
+        <div class="flex flex-row">
+        <div class="h-full w-2/6"></div>
+        <div class="h-72 w-4/6 min-w-[250px] flex flex-row flex-wrap justify-start">
+        <div class="h-72 w-1/2 flex flex-row">
+        <div class="w-2/6 flex flex-col ml-1 justify-start">
+        <p class="w-full text-base font-bold">Product</p>
+        <p class="w-full text-sm">Features</p>
+        <p class="w-full text-sm">Pricing</p>
+
+        </div>
+        <div class="w-2/6"></div>
+        <div class="w-2/6"></div>
+        </div>
+        <div class="j-72 w-1/2"></div>
+        </div>
+        <div class=""></div>
+        </div>
+        <div class="flex h-28">
+
+        </div>
     </div>
 </template>
+<style scoped>
+.footer{
+    
+}
+</style>
